c63e53dceaf4d0d1c9d8b6acaf845ffeb2b8ca7a
destroyer886 <134991440+destroyer886@users.noreply.github.com>
2024-02-21 21:22:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var myanim = new hoverEffect({
+    parent: document.querySelector('.img'),
+    intensity: 0.3,
+    image1: './img_one.jpg',
+    image2: './img_two.jpg',
+    displacementImage: './4.png'
+})
